55339572f114ee6af3c0f62cc9dc0abc7cb57ade
Karthik Siddarth <iamkarthiksiddarth@outlook.com>
2019-06-08 17:12:48 +0530

    initialize component - question and answer screen

---
+++ src/screens/questionAndAnswer/questionAndAnswer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+class QuestionAndAnswer extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Question and Answer</Text>
+      </View>
+    )
+  }
+}
+
+export { QuestionAndAnswer }
