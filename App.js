7226271ec783779171812b2ecf742cdf894dc79d
Karthik Siddarth <iamkarthiksiddarth@outlook.com>
2019-06-16 22:43:31 +0530

    render Root component

--- App.js
+++ App.js
@@ -4,9 +4,7 @@ import Root  from './src/root'
 export default class App extends Component {
   render() {
     return (
-      <View>
-        <Text>ApniKheti App</Text>
-      </View>
+      <Root/>
     );
   }
 }
