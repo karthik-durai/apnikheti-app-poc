6e18ffae8213c30ec55c97a92895f3cd6f9dc739
Karthik Siddarth <iamkarthiksiddarth@outlook.com>
2019-06-16 22:38:17 +0530

    import Screen component and export it

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,3 +1,7 @@
-import { questionAndAnswer  }  from './questionAndAnswer/index'
+import { QuestionAndAnswer }  from './questionAndAnswer/index'
+import { Agriculture } from './agriculture/index'
+import { Home } from './home/index'
+import { LiveStock } from './liveStock/index'
+import { MyProfile } from './myProfile/index'
 
-export { questionAndAnswer  }
+export { QuestionAndAnswer, Agriculture, Home, LiveStock, MyProfile }
