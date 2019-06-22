29897bcff07d6c83af3719880c7e5e5f2671d8db
Karthik Siddarth <iamkarthiksiddarth@outlook.com>
2019-06-22 13:00:01 +0530

    import TabNavigation,pass it to createAppContainer

--- src/root.js
+++ src/root.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
 import { createAppContainer } from 'react-navigation'
 
+import { TabNavigation } from './routes/index'
+
+const App = createAppContainer(TabNavigation)
+
 export default class Root extends Component {
   render() {
     return (
